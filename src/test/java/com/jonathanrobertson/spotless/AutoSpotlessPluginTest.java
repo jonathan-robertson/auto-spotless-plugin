1b830baf184037a69df1f9fca19a747970247295
Jonathan Robertson <jrob356@icloud.com>
2019-11-15 17:57:21 -0600

    update tests to validate absolute path

--- src/test/java/com/jonathanrobertson/spotless/AutoSpotlessPluginTest.java
+++ src/test/java/com/jonathanrobertson/spotless/AutoSpotlessPluginTest.java
@@ -2,13 +2,26 @@ package com.jonathanrobertson.spotless;
 
 import static org.junit.Assert.*;
 
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 public class AutoSpotlessPluginTest {
 
 	@Test
 	public void getAbsolutePathFromEmbeddedFile() {
-		AutoSpotlessPlugin.getAbsolutePathFromEmbeddedFile("spotless.importorder");
-		AutoSpotlessPlugin.getAbsolutePathFromEmbeddedFile("spotless.eclipseformat.xml");
+		final String tempDir = System.getProperty("java.io.tmpdir");
+		Arrays.asList("spotless.importorder", "spotless.eclipseformat.xml").forEach(filename -> {
+			String expected = Paths.get(tempDir, filename).toString();
+			String actual = AutoSpotlessPlugin.getAbsolutePathFromEmbeddedFile(filename).getAbsolutePath();
+			Assert.assertEquals(expected, actual);
+		});
+	}
+
+	@Test
+	public void gitIsPresent() {
+		AutoSpotlessPlugin.gitIsPresent(); // just confirm an unexpected exception doesn't appear
 	}
 }
